7b5052568b1bbddcd9e0fe0660338386ca7af9ea
Lucas Liermann <lucasliermann@hotmail.com>
2023-09-14 16:42:55 -0300

    começo da página
    dia 14.09.2023

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,23 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-  <section className='bg-zinc-600 h-screen text-zinc-200 flex justify-center items-center flex-col gap-10'>
-    <div className='bg-white w-2/5 h-60 p-5 text-red-800 flex items-center justify-center flex-col rounded-full'>
-      <h1 className='text-2xl text-lime-500'>Hello world </h1>
-      <p className='first-letter:uppercase'>oi</p>
-      <button className='border border-blue-950 p-2 rounded-3xl hover:bg-gray-950 transition duration-500 ease-in-out'>clique aqui</button>
-      <Link target='blank' href="https://tailwindcss.com/docs/transition-duration">bom dia</Link>
-    </div>
-    <div>
-      <p className='bg-green-500 p-8 text-fuchsia-700 rounded-3xl h-50' >oi 2</p>
-    </div>
-  </section>
+  <>
+  <header className='bg-blue-400 flex justify-between items-center fixed w-screen'>
+    <h1 className= 'h-[80px] text-3xl flex items-center p-6 font-bold '>
+      Next Page
+    </h1>
+    <nav className='flex gap-[100px]'>
+     oi
+     <div className=' p-6'>
+      enter
+     </div>
+    </nav>
+  </header>
+  <div className='p-7 text-3xl h-[90vh] bg-slate-300 h-screen'>
+    Banner 
+
+  </div>
+  </>
+  
   )
 }
